f033656a56f7de8dd9278a2af915873f4a6fd181
Ornit Cohen Gindi <ornit.cgindi@gmail.com>
2025-04-22 13:25:33 +0300

    action looks good

--- action.py
+++ action.py
@@ -9,49 +9,47 @@ class Action:
         self.y = y
 
     def execute(self):
-        self.add()
-        self.flip()
+        self.__add()
+        self.__flip()
 
 
-    def add(self):
+    def __add(self):
         self.board[self.x][self.y] = self.turn
 
-    def flip(self):
+    def __flip(self):
         self.__flip_horizontal(LEFT)
         self.__flip_horizontal(RIGHT)
         self.__flip_vertical(UP)
         self.__flip_vertical(DOWN)
-        self.__flip_diagonal_main()
-        self.__flip_diagonal_secondary()
+        self.__flip_diagonal(RIGHT, UP)   #main diagonal
+        self.__flip_diagonal(LEFT, DOWN)  #main diagonal
+        self.__flip_diagonal(LEFT, UP)    #second diagonal
+        self.__flip_diagonal(RIGHT, DOWN)  #second diagonal
 
 
     def __flip_horizontal(self, direction):
-        row = self.x
-        col = self.y
-        to_flip = []
-        while col > 0 and col < SIDE_SIZE:
-            col += direction
-            if self.board[row][col] == self.turn:
-                break
-            elif self.board[row][col] == EMPTY:
-                break
-            else:
-                to_flip.append(col)
-        for i in to_flip:
-            self.board[row][i] = self.turn
+        self.__flip(direction, STAY)
 
 
     def __flip_vertical(self, direction):
+        self.__flip(STAY, direction)
+
+    def __flip_diagonal(self, horizontal_direction ,vertical_direction):
+        self.__flip(horizontal_direction, vertical_direction)
+
+
+    def __flip(self, horizontal_direction ,vertical_direction):
         row = self.x
         col = self.y
-        to_flip = []
-        while row > 0 and row < SIDE_SIZE:
-            row += direction
+        to_flip = []  #list of tuples
+        while row > 0 and col > 0:
+            row += vertical_direction
+            col += horizontal_direction
             if self.board[row][col] == self.turn:
                 break
             elif self.board[row][col] == EMPTY:
                 break
             else:
-                to_flip.append(row)
+                to_flip.append((row, col))
         for i in to_flip:
-            self.board[i][col] = self.turn
+            self.board[i[0]][i[1]] = self.turn
--- constants.py
+++ constants.py
@@ -7,3 +7,4 @@ UP = -1
 DOWN = 1
 LEFT = -1
 RIGHT = 1
+STAY = 0
